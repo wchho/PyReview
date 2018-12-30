1960fa84d6a088a8606b971026137d04fd002382
Wilson Ho <wilsonho@Wilsons-MacBook-Pro.local>
2018-12-29 22:47:16 -0500

    exercise, etc.

---
+++ ex11.py
@@ -0,0 +1,8 @@
+print("how old are you?", end= ' ')
+age = input()
+print("How tall are you?", end='\n')
+height = input()
+print("how much do you weigh?", end='\n')
+weight = input()
+
+print(f"So, you're {age} old, {height} fall, and {weight} heavy")
